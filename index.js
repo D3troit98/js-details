8cf522bcccc326f3a70fb3e01ddd32cac253d668
D3troit98 <duruakuebuka@gmail.com>
2022-06-22 22:26:19 +0100

    my details

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById("name").innerHTML = "Name: Duruaku Ebuka"
+
+let height = "180cm"
+let country = "Nigeria"
+ document.getElementById("height").innerHTML= height
+ document.getElementById("country").innerHTML = country
